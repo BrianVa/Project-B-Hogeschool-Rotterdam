8870d7bf0b989542e4c0b662c1535c71308ff4ba
GVisser1 <55433031+GVisser1@users.noreply.github.com>
2020-05-22 16:10:17 +0200

    Zoeken met enter
    Naam films terug naar kleine letters, anders kan je de films blijkbaar niet zoeken.

--- TicketApp/TicketApp/MainApp.Designer.cs
+++ TicketApp/TicketApp/MainApp.Designer.cs
@@ -920,6 +920,7 @@
             // 
             // MainApp
             // 
+            this.AcceptButton = this.search_button;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
